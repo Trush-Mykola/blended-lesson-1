363b58d64b1fa6fd5bdc504b1fb31205092a569d
Satanw8you <trusnikolaj41@gmail.com>
2024-03-10 12:17:47 +0200

    finish task-2

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,3 +1,10 @@
-export const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import style from './Statistics.module.css';
+export const StatisticsItem = ({ statsData: { total, title }, icon: Icon }) => {
+  return (
+    <>
+      <Icon size={30} />
+      <span className={style.counter}>{total}</span>
+      <p className={style.text}>{title}</p>
+    </>
+  );
 };
